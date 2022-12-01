5bd8ff5cc173609aa2ca6c9bee6440387a90d3e4
steven kenneth <stevmili123@gmail.com>
2022-12-01 11:53:55 +0700

    first commit

---
+++ .history/src/components/Footer/Footer_20221124171520.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+import honda from "../../assets/all-images/honda.png";
+import ins from "../../assets/all-images/ins.webp";
+import wht from "../../assets/all-images/wht.webp";
+
+const quickLinks = [
+  {
+    path: "/about",
+    display: "About",
+    
+  },
+  {
+    path: "/cars",
+    display: "Car Listing",
+  },
+  {
+    path: "/blogs",
+    display: "Artikel Terbaru",
+  },
+
+ 
+];
+
+
+const Footer = () => {
+  
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo footer__logo">
+              <h1>
+                <Link to="/home" className=" d-flex align-items-center gap-2">
+                <img className="honda" src={honda} alt="" />
+                    <i></i>
+                    <span>HONDA
+                     <br/> 
+                    </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+            Honda Auto Tangerang - Melayani penjualan mobil secara Cash dan Credit untuk All-Type Mobil Honda. Harga yang tertera belum masuk promo diskon atau cashback yang tersedia, dan harga tersebut dapat berubah kapan saja.
+            </p>
+          </Col>
+
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Head Office</h5>
+              <p className="office__info">Jl. Raya Serang No.KM14, Dukuh, Kec. Cikupa, Tangerang, Banten 15710</p>
+              <p className="office__info">Phone: +62 812-1222-3099</p>
+
+              <p className="office__info">Email: evas24453@gmail.com </p>
+
+              <p className="office__info">Office Time: 10am - 7pm</p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="12">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Follow Us</h5>
+              <div className=" d-flex align-items-center gap-4 mt-3">
+                  <div className="social__link-icon">
+                  <a href="https://www.instagram.com/promohondatangerang_id/" className="maps" target="_blank" rel="noreferrer">
+
+                  <img className="follow" src={ins} alt="" />
+                  </a>
+                  <br />
+                  <br />
+                  <a href="https://api.whatsapp.com/send?phone=6281212223099&text=Saya%20dari%20website%20Honda%20Auto%20Tangerang%20%0Anama%20%20%3A%0Atujuan%20%3A" className="maps" target="_blank" rel="noreferrer">
+                  <img className="follow" src={wht} alt="" />
+                  </a>
+                </div>
+                </div>
+            </div>
+          </Col>
+
+          
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ .history/src/pages/About_20221201111840.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import AboutSection1 from "../components/UI/AboutSection1";
+import { Container, Row, Col } from "reactstrap";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+
+import driveImg from "../assets/all-images/drive.webp";
+import OurMembers from "../components/UI/OurMembers";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <Helmet title="About">
+      <CommonSection title="Tentang Kami" />
+      <AboutSection1 aboutClass="aboutPage" />
+
+      <section className="about__page-section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section__title">
+                  Honda Auto Tangerang
+                </h2>
+
+                <p className="section__description">
+                Honda Auto Tangerang - Melayani penjualan mobil secara Cash dan Credit untuk All-Type Mobil Honda. Harga yang tertera belum masuk promo diskon atau cashback yang tersedia, dan harga tersebut dapat berubah kapan saja.
+                </p>
+
+                <p className="section__description">
+                Pembelian yang dilakukan secara Credit, Anda dapat memilih pembiayaan sendiri atau sesuai rekomendasi dari Sales Consultant Terbaik. Silahkan menghubungi Kontak Sales terbaik kami pada salah satu media komunikasi yang telah di sediakan di Website ini yakni melalui telepon, WhatsApp , email, ataupun kontak social media yang telah tertera.
+                </p>
+
+                <div className=" d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section__subtitle">Need Any Help?</h6>
+                    <h4>+62 812-1222-3099</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <BecomeDriverSection />
+
+      {/* <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="section__subtitle">Experts</h6>
+              <h2 className="section__title">Our Members</h2>
+            </Col>
+            <OurMembers />
+          </Row>
+        </Container>
+      </section> */}
+    </Helmet>
+  );
+};
+
+export default About;
